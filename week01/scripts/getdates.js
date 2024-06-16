963c1f37037f6ed7d98f8326f9f57dc5a699216d
JeanFrancoLeonB/wdd131 <>
2024-06-16 00:20:30 -0500

    week01

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,9 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+
+document.querySelector("#lastupdated").textContent = `Last Modification: ${document.lastModified}`;
+
+
+//document.getElementById("copyrightyear").textContent = new Date().getFullYear();;
+//document.querySelector("#copyrightyear").textContent = new Date().getFullYear();;
